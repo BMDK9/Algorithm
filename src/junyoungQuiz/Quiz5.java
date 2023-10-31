2f3f6ca6b92b9017a9cedaba2a0352b8887c6dbe
BMDK9 <qnrakdlzm77@naver.com>
2023-10-31 17:47:42 +0900

    Quiz5

---
+++ src/junyoungQuiz/Quiz5.java
@@ -0,0 +1,27 @@
+package junyoungQuiz;
+
+import java.util.Scanner;
+
+public class Quiz5 {
+    public static void main(String[] args) {
+        System.out.println("1~10까지의 수를 5번 입력 받아서 가장 많이 입력된 수를 구하세요!");
+        int[] arr = new int[11];       // 담을 배열 생성
+
+        for (int i = 0; i < 5; i++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print((i + 1) + "번째 수: ");
+            int j = sc.nextInt();
+            arr[j] = arr[j] + 1;
+        }                                       // 숫자별 입력 횟수 배열로 카운트
+
+        int max = 0;
+        int result = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                result = i;
+            }
+        }
+        System.out.println("가장 많이 입력된 수는 "+result);
+    }
+}
--- src/programers/lv0/GetAge.java
+++ src/programers/lv0/GetAge.java
@@ -2,11 +2,12 @@ package programers.lv0;
 
 public class GetAge {
 
-//    public int solution(int age) {
-//
-//        int year = 2022;
-//        if (0 < age && age <= 120) {
-//            int brithYear = year-age;
-//        }
-//    }
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2022;
+        if (0 < age && age <= 120) {
+            int brithYear = year-age;
+        }
+        return answer;
+    }
 }

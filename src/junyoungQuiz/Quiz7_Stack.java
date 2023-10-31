083de25234385059bd8511dda1f06ad66e780bce
BMDK9 <qnrakdlzm77@naver.com>
2023-10-31 20:26:41 +0900

    Quiz7

--- src/junyoungQuiz/Quiz7_Stack.java
+++ src/junyoungQuiz/Quiz7_Stack.java
@@ -1,5 +1,24 @@
 package junyoungQuiz;
 
-public class Quiz7_Stack {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Quiz7_Stack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inputTime = sc.nextInt();
+        System.out.println("=========================");
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < inputTime; i++) {
+            int inputNumber = sc.nextInt();
+            if (inputNumber == 0 ) {
+                stack.pop();
+            } else {
+                stack.push(inputNumber);
+            }
+        }
+        System.out.println(stack.peek());
+    }
 }
--- src/programers/lv0/GetAge.java
+++ src/programers/lv0/GetAge.java
@@ -5,9 +5,11 @@ public class GetAge {
     public int solution(int age) {
         int answer = 0;
         int year = 2022;
+        int brithYear = 0;
         if (0 < age && age <= 120) {
-            int brithYear = year-age;
+            brithYear = year - age;
         }
+        answer = brithYear;
         return answer;
     }
 }

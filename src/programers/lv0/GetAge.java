208053fdd23a3e3c10dd4ac43a19b2fe2c06e96c
BMDK9 <qnrakdlzm77@naver.com>
2023-11-01 10:52:11 +0900

    Completed GetAge

--- src/programers/lv0/GetAge.java
+++ src/programers/lv0/GetAge.java
@@ -1,15 +1,15 @@
 package programers.lv0;
 
 public class GetAge {
-
-    public int solution(int age) {
+    public static void main(String[] args) {
+        System.out.println(solution(41));
+    }
+    public static int solution(int age) {
         int answer = 0;
         int year = 2022;
-        int brithYear = 0;
         if (0 < age && age <= 120) {
-            brithYear = year - age;
+            answer = year - age + 1;
         }
-        answer = brithYear;
         return answer;
     }
 }

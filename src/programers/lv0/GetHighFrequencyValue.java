db9aa3d6fbd84ecf18129a6baa97010cea2b3796
BMDK9 <qnrakdlzm77@naver.com>
2023-11-01 14:57:39 +0900

    Quiz9

--- src/programers/lv0/GetHighFrequencyValue.java
+++ src/programers/lv0/GetHighFrequencyValue.java
@@ -1,22 +1,26 @@
 package programers.lv0;
 
+import java.util.ArrayList;
+
 public class GetHighFrequencyValue {
     public int solution(int[] array) {
         int answer = 0;
 
-//        int i = array
-//        int j = arr
-
-        int[] arr = new int[1001];
-        for (int i = 0; i < 100; i++) {
-            for (int j = 1; j < 1001; j++) {
-
-                arr[j] = arr[j] + 1;
+        int[] inputNumber = new int[1001];              // 0~1000까지의 숫자
+        for (int i = 0; i < array.length; i++) {         //100번 입력
+            if (array[i] >= 0 && array[i] <= 1000) {
+                inputNumber[array[i]] += 1;                    // 0~1000까지 카운트
             }
-
         }
-
-
+        int max = 0;
+        int manyNumber = 0;
+        for (int i = 0; i < inputNumber.length; i++) {
+            if (max < inputNumber[i]) {
+                max = inputNumber[i];
+                manyNumber = i;
+            }
+        }
+        answer = manyNumber;
         return answer;
     }
 }

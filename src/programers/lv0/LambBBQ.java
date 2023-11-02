24c0378a70d6d9ac5969f4493ccdd0ff35a244d4
BMDK9 <qnrakdlzm77@naver.com>
2023-11-02 21:36:45 +0900

    알고리즘 시간

--- src/programers/lv0/LambBBQ.java
+++ src/programers/lv0/LambBBQ.java
@@ -3,11 +3,13 @@ package programers.lv0;
 public class LambBBQ {
     public int solution(int n, int k) {
         int service = 0;
-        if (n/10==0) {
+        int a = n;
+        while ( a - 10 >= 0) {
+            a -= 10;
             service += 1;
         }
-        int lamb = n*12000;
-        int drink = (k-service)*2000;
+        int lamb = n * 12000;
+        int drink = (k - service) * 2000;
 
         return lamb + drink;
     }

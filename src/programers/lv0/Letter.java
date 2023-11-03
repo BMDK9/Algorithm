c309392c5fbda78dc73127f469987c78965b9a3d
BMDK9 <qnrakdlzm77@naver.com>
2023-11-03 20:55:02 +0900

    편지 길이 구하기

--- src/programers/lv0/Letter.java
+++ src/programers/lv0/Letter.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public class Letter {
     public int solution(String message) {
-        char[] characters = message.toCharArray();              // 메세지를 char의 배열로 변경
+        char[] characters = message.toCharArray();              // 메세지를 문자의 배열로 변경
         return characters.length * 2;                           // 배열의 길이 * 2
     }
+    // return message.length() * 2;                             이렇게 해도 문자의 개수를 구할 수 있다.
+
 }

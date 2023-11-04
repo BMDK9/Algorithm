25d675425d46729e2f88c82cbbeac909d8bcda89
BMDK9 <qnrakdlzm77@naver.com>
2023-11-04 10:39:52 +0900

    배열 원소의 길이

---
+++ src/programers/lv0/ArrayElementsLength.java
@@ -0,0 +1,23 @@
+package programers.lv0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayElementsLength {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];                 // 배열의 길이를 매개변수 길이만큼
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();                    // for문 으로 변수 값 돌면서 입력
+        }
+            return answer;
+    }
+}
+//    public List<Integer> solution(String[] strlist) {         // 리스트를 이용한 다른 풀이
+//        List<Integer> answer = new ArrayList<>();
+//        for (String words : strlist) {
+//            answer.add(words.length());
+//        }
+//        return answer;
+//    }
+//}
+

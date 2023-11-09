ab235030af54b5cb6fb14956d4465710ff9d1d6f
BMDK9 <qnrakdlzm77@naver.com>
2023-11-10 00:30:58 +0900

    자연수 뒤집어 배열로 만들기

--- src/programers/lv1/자연수뒤집어배열로만들기.java
+++ src/programers/lv1/자연수뒤집어배열로만들기.java
@@ -1,14 +1,35 @@
 package programers.lv1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class 자연수뒤집어배열로만들기 {
+    public static void main(String[] args) {
+        solution(12345);
+    }
 
-    public int[] solution(long n) {
-        List<Character> characters = new ArrayList<>();
+    public static int[] solution(long n) {
 
+        String a = Long.toString(n);
+        int[] answer = new int[a.length()];
 
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = (int) (n % 10);
+            n /= 10;
+        }
         return answer;
+
+//        String string_n = Long.toString(n);           // 변환하는 것으로 푼 것
+//        List<Character> char_n = new ArrayList<>();
+//        int[] answer = new int[string_n.length()];
+//
+//        for (int i = string_n.length(); i > 0; i--) {
+//            char_n.add(string_n.charAt(i-1));
+//        }
+//
+//        for (int i = 0; i < char_n.size(); i++) {
+//            answer[i] = Character.getNumericValue(char_n.get(i));
+//        }
+//
+//        return answer;
     }
 }

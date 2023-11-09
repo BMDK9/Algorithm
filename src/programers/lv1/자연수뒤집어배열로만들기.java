342ef92e32dd2096632c330f61a1df6d8d461f6f
BMDK9 <qnrakdlzm77@naver.com>
2023-11-09 23:19:43 +0900

    자연수 뒤집어 배열로 만들기

---
+++ src/programers/lv1/자연수뒤집어배열로만들기.java
@@ -0,0 +1,14 @@
+package programers.lv1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 자연수뒤집어배열로만들기 {
+
+    public int[] solution(long n) {
+        List<Character> characters = new ArrayList<>();
+
+
+        return answer;
+    }
+}

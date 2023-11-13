900bf30abfc558fe9a6cfce24b7ce83de3af540e
BMDK9 <qnrakdlzm77@naver.com>
2023-11-13 12:13:07 +0900

    햄버거 만들기

--- src/programers/lv1/햄버거만들기.java
+++ src/programers/lv1/햄버거만들기.java
@@ -6,21 +6,34 @@ import java.util.Stack;
 
 public class 햄버거만들기 {
     public static void main(String[] args) {
-        int[] a = {4, 1, 2, 3, 1, 5, 1, 2, 3, 1};
+        int[] a = {1, 3, 2, 1, 2, 1, 3, 1, 2};
         System.out.println(solution(a));
     }
 
     public static int solution(int[] ingredient) {
         int answer = 0;
-        Stack ingredi = new Stack<>();
-        Stack supervise = new Stack<>();
+        Stack<Integer> ingredi = new Stack<>();
+        Stack<Integer> supervise = new Stack<>();
+        int[] hamburger = {1, 3, 2, 1};
 
         for (int make : ingredient) {
+            ingredi.push(make);
             if (ingredi.size() >= 4 && ingredi.peek().equals(1)) {
+                boolean success = true;
                 for (int i = 0; i < 4; i++) {
                     supervise.push(ingredi.pop());
+                    if (hamburger[i] != supervise.peek()) {
+                        while (!supervise.isEmpty()) {
+                            ingredi.push(supervise.pop());
+                        }
+                        success = false;
+                        break;
+                    }
                 }
-
+                supervise.clear();
+                if (success) {
+                    answer++;
+                }
             }
         }
         return answer;

27f5c2e3d0e8cec2f0242edac3a9e9f601fe0655
BMDK9 <qnrakdlzm77@naver.com>
2023-11-13 22:40:31 +0900

    햄버거 만들기

--- src/programers/lv1/햄버거만들기.java
+++ src/programers/lv1/햄버거만들기.java
@@ -12,27 +12,27 @@ public class 햄버거만들기 {
 
     public static int solution(int[] ingredient) {
         int answer = 0;
-        Stack<Integer> ingredi = new Stack<>();
-        Stack<Integer> supervise = new Stack<>();
-        int[] hamburger = {1, 3, 2, 1};
+        Stack<Integer> ingredi = new Stack<>();             // 재료를 받아올 스택
+        Stack<Integer> supervise = new Stack<>();           // 재료 순서를 검증할 스택
+        int[] hamburger = {1, 3, 2, 1};                     // 햄버거를 완성하기 위한 재료의 순서
 
-        for (int make : ingredient) {
+        for (int make : ingredient) {                       // 재료를 받아옴
             ingredi.push(make);
-            if (ingredi.size() >= 4 && ingredi.peek() == hamburger[0]) {
-                boolean success = true;
+            if (ingredi.size() >= 4 && ingredi.peek() == hamburger[0]) {    // 검증하기 위한 조건
+                boolean success = true;                     // 검증 결과 태그
 
-                for (int j : hamburger) {
+                for (int j : hamburger) {                   // 검증 실패 조건
                     if (j != ingredi.peek()) {
                         success = false;
                         break;
                     }
-                    supervise.push(ingredi.pop());
+                    supervise.push(ingredi.pop());          // 각 재료의 검증 완료 시
                 }
-                if (success) {
+                if (success) {                              // 모든 재료의 검증 완료 시
                     answer++;
                     supervise.clear();
                 } else {
-                    while (!supervise.isEmpty()) {
+                    while (!supervise.isEmpty()) {          // 검증 실패시 공정과정
                         ingredi.push(supervise.pop());
                     }
                 }

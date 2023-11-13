795526b5f83851256ef9fc10be3f9760862569a5
BMDK9 <qnrakdlzm77@naver.com>
2023-11-13 23:41:20 +0900

    정수 제곱근 판별

--- src/programers/lv1/정수제곱근판별.java
+++ src/programers/lv1/정수제곱근판별.java
@@ -5,6 +5,12 @@ public class 정수제곱근판별 {
 
         long root = (long) Math.sqrt(n);
         return Math.sqrt(n)%1 == 0 ? (long) Math.pow(root + 1, 2) : -1;
+//		아래와 같은 의미이다.
+//        if (Math.sqrt(n) % 1 == 0) {
+//            return = (long) Math.pow(root+1, 2);
+//        } else {
+//            return -1;
+//        }
 
     }
 }

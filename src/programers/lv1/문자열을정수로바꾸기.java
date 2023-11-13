dc29b48b22559bf918d8bc647fbfaef8895674c8
BMDK9 <qnrakdlzm77@naver.com>
2023-11-13 21:34:40 +0900

    문자열을 정수로 바꾸기

---
+++ src/programers/lv1/문자열을정수로바꾸기.java
@@ -0,0 +1,11 @@
+package programers.lv1;
+
+public class 문자열을정수로바꾸기 {
+    public static void main(String[] args) {
+    int a = -123;
+        System.out.println(a);
+    }
+    public int solution(String s) {
+        return Integer.parseInt(String.valueOf(s));
+    }
+}

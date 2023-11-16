bd45b8d1d2e32b45fd32e9ec1a99d3f49d72d4ff
BMDK9 <qnrakdlzm77@naver.com>
2023-11-16 20:15:55 +0900

    정수 내림차순으로 배치하기

--- src/programers/lv1/정수내림차순으로배치하기.java
+++ src/programers/lv1/정수내림차순으로배치하기.java
@@ -18,9 +18,26 @@ public class 정수내림차순으로배치하기 {
             answer += (long) (intN.get(i) * (Math.pow(10,i)));
         }
         return answer;
-
-//        char[] nlong = Long.toString(n).toCharArray();
-//        Arrays.sort(nlong);
-//        return Long.parseLong(new StringBuilder(new String(nlong)).reverse().toString());
     }
+//      오름차순으로
+//    public long ChatGPT(long n) {
+//        List<Integer> digits = new ArrayList<>();
+//
+//        while (n != 0) {
+//            digits.add((int) (n % 10));
+//            n /= 10;
+//        }
+//
+//        Collections.sort(digits);
+//
+//        long answer = 0;
+//        long multiplier = 1;
+//
+//        for (int i = digits.size() - 1; i >= 0; i--) {
+//            answer += digits.get(i) * multiplier;
+//            multiplier *= 10;
+//        }
+//
+//        return answer;
+//    }
 }

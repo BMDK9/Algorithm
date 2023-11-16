9aedfe57571ea89415511179d3a0f5b42ec630b6
BMDK9 <qnrakdlzm77@naver.com>
2023-11-16 19:46:56 +0900

    정수 내림차순으로 배치하기

---
+++ src/programers/lv1/정수내림차순으로배치하기.java
@@ -0,0 +1,26 @@
+package programers.lv1;
+
+
+import java.util.*;
+
+public class 정수내림차순으로배치하기 {
+
+    public long solution(long n) {
+        long answer = 0;
+        List<Integer> intN = new ArrayList<>();
+        while (n != 0) {
+            intN.add((int) (n % 10));
+            n /= 10;
+        }
+        Collections.sort(intN);
+
+        for (int i = intN.size(); i > 0; i--) {
+            answer += (long) (intN.get(i-1) * (Math.pow(10,i-1)));
+        }
+        return answer;
+
+//        char[] nlong = Long.toString(n).toCharArray();
+//        Arrays.sort(nlong);
+//        return Long.parseLong(new StringBuilder(new String(nlong)).reverse().toString());
+    }
+}

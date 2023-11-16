14563e7aa51b818d4f83bcd14d25ae78f6960fd0
BMDK9 <qnrakdlzm77@naver.com>
2023-11-16 20:48:41 +0900

    콜라츠추측

---
+++ src/programers/lv1/콜라츠추측.java
@@ -0,0 +1,21 @@
+package programers.lv1;
+
+public class 콜라츠추측 {
+    public int solution(int num) {
+        int answer = 0;
+        long lnum = num;
+        while (lnum != 1) {
+            if (answer == 500) {
+                answer = -1;
+                break;
+            } else if (lnum % 2 == 0) {
+                lnum /= 2;
+                answer++;
+            } else {
+                lnum = lnum * 3 + 1;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}

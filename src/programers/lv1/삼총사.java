538548f5059b8483c9f1a17b6a5514b3052e6b64
BMDK9 <qnrakdlzm77@naver.com>
2023-11-16 19:46:46 +0900

    삼총사

--- src/programers/lv1/삼총사.java
+++ src/programers/lv1/삼총사.java
@@ -8,13 +8,9 @@ public class 삼총사 {
     public int solution(int[] number) {
         int answer = 0;
         int students = number.length;
-
         for (int i = 0; i < students - 2; i++) {
-
             for (int j = i + 1; j < students - 1; j++) {
-
                 for (int k = j + 1; k < students; k++) {
-
                     if (number[i] + number[j] + number[k] == 0) {
                         answer++;
                     }

19ad75daa158594cc6f781e5543428f4ef87ac67
BMDK9 <qnrakdlzm77@naver.com>
2023-11-17 09:55:10 +0900

    나누어 떨어지는 숫자 배열

---
+++ src/programers/lv1_15_20/나누어떨어지는숫자배열.java
@@ -0,0 +1,29 @@
+package programers.lv1_15_20;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class 나누어떨어지는숫자배열 {
+    public int[] solution(int[] arr, int divisor) {
+        List<Integer> divNum = new LinkedList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                divNum.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[divNum.size()];
+
+        if (divNum.isEmpty()) {
+            return new int[]{-1};
+        } else {
+            for (int i = 0; i < divNum.size(); i++) {
+                answer[i] = divNum.get(i);
+            }
+            Arrays.sort(answer);
+        }
+        return answer;
+    }
+}

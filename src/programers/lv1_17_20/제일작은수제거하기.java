81f620a361f16c413cbee3559a92d099d34ff19a
BMDK9 <qnrakdlzm77@naver.com>
2023-11-22 01:45:16 +0900

    제일 작은 수 제거하기

--- src/programers/lv1_17_20/제일작은수제거하기.java
+++ src/programers/lv1_17_20/제일작은수제거하기.java
@@ -13,7 +13,6 @@ public class 제일작은수제거하기 {
         }
 
         int[] answer = new int[arr.length - 1];
-
         int j = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != min) {

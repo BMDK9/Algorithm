caac1d926b89dc73d8ab82db1d389967b5effc9c
BMDK9 <qnrakdlzm77@naver.com>
2023-11-23 09:16:51 +0900

    clean : lv1 20문제 채움

---
+++ src/programers/lv1_20/모의고사.java
@@ -0,0 +1,25 @@
+package programers.lv1_20;
+
+public class 모의고사 {
+    public int[] solution(int[] answers) {
+//        int[] omrCard = new int[answers.length];
+        int i = 0;
+        int su1 = 1;
+        int su2 = 2;
+        int[] count = new int[3];
+
+        while (i == answers.length) {
+            if (answers[i] == su1) count[0]++;
+            su1++;
+            if (su1 == 6) su1 = 1;
+//            =========== 수포자 1
+            if (i % 2 == 1) su2 = 2;
+
+
+
+            if (answers[i] == su2) count[1]++;
+
+        }
+    }
+}
+}

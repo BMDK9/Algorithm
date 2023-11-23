5c2b421ab2e023a87bcc6c8b143d44493b6b83fa
BMDK9 <qnrakdlzm77@naver.com>
2023-11-23 11:08:00 +0900

    feat : 부족한 금액 계산하기

--- src/programers/lv1_21_40/부족한금액계산하기.java
+++ src/programers/lv1_21_40/부족한금액계산하기.java
@@ -3,11 +3,12 @@ package programers.lv1_21_40;
 public class 부족한금액계산하기 {
     public static long solution(int price, int money, int count) {
         long sum = 0;
-        long answer = money - sum;
 
         for (int i = 1; i <= count; i++) {
             sum += price * i;
         }
+
+        long answer = money - sum;
 
         if (sum > money) {
             return answer;

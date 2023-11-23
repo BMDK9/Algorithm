acbc398b000ac8a84676cd259513b7ed70982058
BMDK9 <qnrakdlzm77@naver.com>
2023-11-23 11:16:25 +0900

    clean : 부족한 금액 계산하기

--- src/programers/lv1_21_40/부족한금액계산하기.java
+++ src/programers/lv1_21_40/부족한금액계산하기.java
@@ -1,19 +1,17 @@
 package programers.lv1_21_40;
 
 public class 부족한금액계산하기 {
-    public static long solution(int price, int money, int count) {
-        long sum = 0;
+    public long solution(int price, int money, int count) {
 
+        long sum = 0;
         for (int i = 1; i <= count; i++) {
             sum += price * i;
         }
 
-        long answer = money - sum;
-
         if (sum > money) {
-            return answer;
-        } else {
-            return 0;
+            return sum - money;
         }
+        return 0;
     }
 }
+

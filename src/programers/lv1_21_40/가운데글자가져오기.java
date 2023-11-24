13e391bc487294d950d5f8db79de3b0d1f9869d7
BMDK9 <qnrakdlzm77@naver.com>
2023-11-24 09:55:48 +0900

    feat : 가운데 글자 가져오기

---
+++ src/programers/lv1_21_40/가운데글자가져오기.java
@@ -0,0 +1,16 @@
+package programers.lv1_21_40;
+
+public class 가운데글자가져오기 {
+
+    public String solution(String s) {
+        String answer = "";
+
+        if (s.length() % 2 == 0) {
+            answer = String.valueOf(s.charAt((s.length() / 2) - 1));
+        }
+
+        answer += String.valueOf(s.charAt(s.length()/2));
+
+        return answer;
+    }
+}

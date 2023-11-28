a1c3aa1cab8beeda3a6d587ef215d37528fb7659
BMDK9 <qnrakdlzm77@naver.com>
2023-11-28 16:07:26 +0900

    feat : 명예의 전당

--- src/programers/lv1_21_40/명예의전당.java
+++ src/programers/lv1_21_40/명예의전당.java
@@ -7,20 +7,21 @@ import java.util.List;
 public class 명예의전당 {
 
     public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
-        List<Integer> singer = new ArrayList<>();
+//      하루에 한명씩 노래를 부른다. 7일 = 7명, 구할 것은 k 순위의 점수
+        int[] answer = new int[score.length];   // k 순위 점수를 입력할 배열
+        List<Integer> singer = new ArrayList<>();   //  점수를 핸들링할 배열
 
         for (int i = 0; i < score.length; i++) {
-            singer.add(score[i]);
-            Collections.sort(singer);
-            Collections.reverse(singer);
+            singer.add(score[i]);   // 핸들링 배열에 점수 입력
+            Collections.sort(singer);   // 점수를 오름차순으로 정렬
+            Collections.reverse(singer);    // 정렬한 배열을 뒤집음 = 내림차순
 
-            if (i < k) {
-                answer[i] = singer.get(i);
+            if (i < k) {    //  노래 부른 가수가 순위를 매길 k보다 작을 때
+                answer[i] = singer.get(i);  //  i = 제일 낮은 점수를 순위 정렬 배열에 넣는다.
                 continue;
             }
 
-            answer[i] = singer.get(k - 1);
+            answer[i] = singer.get(k - 1);  //  핸들링 배열에 입력된 k 순위의 점수를 입력한다.
         }
         return answer;
     }

d48863c782fb31864811e259c5d5c444c93ea3da
BMDK9 <qnrakdlzm77@naver.com>
2023-11-28 15:52:55 +0900

    feat : 콜라 문제

--- src/programers/lv1_21_40/콜라문제.java
+++ src/programers/lv1_21_40/콜라문제.java
@@ -5,23 +5,24 @@ public class 콜라문제 {
         solution(2, 1, 20);
     }
 
-    public static int solution(int a, int b, int n) {
-//      줘야하는 빈병 a, 콜라 b, 소유중인 빈병 n
-        int answer = 0; // 받은 콜라병의 총 개수
+    public int solution(int a, int b, int n) {
+//      매점에 줘야하는 빈병 a, 새로 받는 콜라 b, 소유중인 빈병 n
+//      새로 받은 콜라는 빈병이 된다.
+        int answer = 0; // 받은 콜라병의 총 개수 = b의 누적
         int ownbottle = n;
 
         while (ownbottle >= a) {
-
+//          리팩토링
             answer += b * (ownbottle / a);
             ownbottle = b * (ownbottle / a) + (ownbottle % a);
 
+//            최초 풀이
 //            if(ownbottle - a >= 0) {
 //                ownbottle -= a;
 //                ownbottle += b;
 //                answer += b;
 //            }
         }
-        System.out.println(answer);
         return answer;
     }
 }

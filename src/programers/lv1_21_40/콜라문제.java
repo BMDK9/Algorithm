dafe440c796f909b114b8eb3e688499ed1e4eb86
BMDK9 <qnrakdlzm77@naver.com>
2023-11-29 11:37:30 +0900

    feat : 콜라 문제

--- src/programers/lv1_21_40/콜라문제.java
+++ src/programers/lv1_21_40/콜라문제.java
@@ -1,10 +1,6 @@
 package programers.lv1_21_40;
 
 public class 콜라문제 {
-    public static void main(String[] args) {
-        solution(2, 1, 20);
-    }
-
     public int solution(int a, int b, int n) {
 //      매점에 줘야하는 빈병 a, 새로 받는 콜라 b, 소유중인 빈병 n
 //      새로 받은 콜라는 빈병이 된다.

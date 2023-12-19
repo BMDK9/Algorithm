06aa32f60dfe97091ec9d84cebb408a4037feb04
BMDK9 <qnrakdlzm77@naver.com>
2023-12-19 22:00:35 +0900

    시저 암호

---
+++ src/programers/lv1_21_40/시저암호.java
@@ -0,0 +1,28 @@
+package programers.lv1_21_40;
+
+public class 시저암호 {
+
+    public String solution(String s, int n) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (char A : chars) {
+            if (A == ' ') {
+                sb.append(" ");
+                continue;
+            }
+            char ch = ' ';
+            if (A >= 65 && A <= 90) {
+                ch = 'A';
+            }
+            if (A >= 97 && A <= 122) {
+                ch = 'a';
+            }
+
+            A += n;
+            char B = (char) (ch + (A - ch) % 26);
+            sb.append(B);
+        }
+        return sb.toString();
+    }
+}

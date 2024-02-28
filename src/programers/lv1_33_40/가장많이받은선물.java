08856a855460596dfeaf8c12dcb96f1e0728281f
BMDK9 <qnrakdlzm77@naver.com>
2024-02-28 21:23:37 +0900

    refactor : 가장 많이 받은 선물 Kakao + 준영이 풀이

--- src/programers/lv1_33_40/가장많이받은선물.java
+++ src/programers/lv1_33_40/가장많이받은선물.java
@@ -60,20 +60,80 @@ public class 가장많이받은선물 {
                         continue;
                     }
                     whoBow[j] += 1;
+                    continue;
                 }
                 // 두 사람이 선물을 주고받은 기록이 있다면
                 if (a > b) {
                     whoBow[i] += 1;
                     continue;
                 }
-                if (a < b) {
-                    whoBow[j] += 1;
-                }
+                whoBow[j] += 1;
             }
             if (whoBow[i] > answer) {
                 answer = whoBow[i];
             }
         }
         return answer;
+    }
+}
+
+class Solution { // 우리 준영이 풀이~
+
+    static HashMap<String, Integer> giftMap = new HashMap<>();
+    static HashMap<String, HashMap<String, Integer>> table = new HashMap<>();
+
+    public int solution(String[] friends, String[] gifts) {
+        initMap(friends);
+        giftPoint(gifts);
+        int answer = 0;
+        for (String name : table.keySet()) {
+            int count = 0;
+            for (String friend : friends) {
+                if (name.equals(friend)) {
+                    continue;
+                }
+                int myPoint = table.get(name).get(friend);
+                int friendPoint = table.get(friend).get(name);
+                if (myPoint > friendPoint) {
+                    count++;
+                    continue;
+                }
+                if (myPoint == friendPoint) {
+                    if (giftMap.get(name) > giftMap.get(friend)) {
+                        count++;
+                    }
+                }
+            }
+            answer = Math.max(count, answer);
+        }
+        return answer;
+    }
+
+    static void initMap(String[] friends) {
+        for (String friend : friends) {
+            table.put(friend, new HashMap<>());
+            giftMap.put(friend, 0);
+        }
+        for (String key : table.keySet()) {
+            for (String friend : friends) {
+                if (key.equals(friend)) {
+                    continue;
+                }
+                table.get(key).put(friend, 0);
+            }
+        }
+    }
+
+    static void giftPoint(String[] gifts) {
+        StringTokenizer st;
+        for (String gift : gifts) {
+            st = new StringTokenizer(gift);
+            String pub = st.nextToken();
+            String sub = st.nextToken();
+
+            table.get(pub).put(sub, table.get(pub).getOrDefault(sub, 0) + 1);
+            giftMap.put(pub, giftMap.get(pub) + 1);
+            giftMap.put(sub, giftMap.get(sub) - 1);
+        }
     }
 }

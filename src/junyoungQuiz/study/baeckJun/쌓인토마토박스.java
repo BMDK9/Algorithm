9e3f2ec598c7cb735199437186bdf089e5f3c121
BMDK9 <qnrakdlzm77@naver.com>
2024-03-05 20:01:33 +0900

    쌓인 토마토 박스(ing)

--- src/junyoungQuiz/study/baeckJun/쌓인토마토박스.java
+++ src/junyoungQuiz/study/baeckJun/쌓인토마토박스.java
@@ -3,40 +3,66 @@ package junyoungQuiz.study.baeckJun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class 쌓인토마토박스 {
 
-    static Map<Integer, int[][]> boxes;
-    static Map<Integer, boolean[][]> dailyChecks;
-    static int[][] box;
-    static boolean[][] dailyCheck;
+    static class Node {
+
+        int x;
+        int y;
+        int z;
+
+        public Node(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int day;
+    static int[][][] box;
+    static boolean[][][] dailyCheck;
 
     public static void main(String[] args) throws IOException {
 
         init();
 
+        for (int i = 0; i < box.length; i++) {
+            for (int j = 0; j < box[i].length; j++) {
+                for (int k = 0; k < box[i][j].length; k++) {
+                    if (box[i][j][k] == -1) {
+                        continue;
+                    }
+                    bfs(i, j, k);
+                }
+            }
+        }
+
+    }
+    private static void bfs(int x, int y, int z) {
+        Node node = new Node(x, y, z);
     }
 
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken()); // 열
-        int n = Integer.parseInt(st.nextToken()); // 오
-        int h = Integer.parseInt(st.nextToken()); // 층
-        boxes = new HashMap<>();
-        dailyChecks = new HashMap<>();
+        int m = Integer.parseInt(st.nextToken()); // x축
+        int n = Integer.parseInt(st.nextToken()); // y축
+        int h = Integer.parseInt(st.nextToken()); // z축
 
-        box = new int[n][m];
-        dailyCheck = new boolean[n][m];
-
-        for (int j = 0; j < n; j++) {
-            st = new StringTokenizer(br.readLine());
-            for (int k = 0; k < m; k++) {
-                box[j][k] = Integer.parseInt(st.nextToken());
+        box = new int[n][m][h];
+        dailyCheck = new boolean[n][m][h];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < m; k++) {
+                    box[j][k][i] = Integer.parseInt(st.nextToken());
+                }
             }
         }
     }
+
+
 }

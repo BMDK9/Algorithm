c391eaa5a6baa7692e81624aa8f39893f1bde401
BMDK9 <qnrakdlzm77@naver.com>
2024-03-06 00:02:47 +0900

    토마토

--- src/junyoungQuiz/study/baeckJun/토마토.java
+++ src/junyoungQuiz/study/baeckJun/토마토.java
@@ -3,6 +3,8 @@ package junyoungQuiz.study.baeckJun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 토마토 {
@@ -18,61 +20,87 @@ public class 토마토 {
         }
     }
 
-    static int day;
+    static int day = 0;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int[][] box;
-    static boolean[][] checked;
+    //    static boolean[][] checked;   // 과거 3
+    static Queue<Node> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         init();
-//        while (true) {
-        for (int i = 0; i < box.length; i++) {
-            for (int j = 0; j < box[i].length; j++) {
-                if (box[i][j] == 1) {
-                    bfs(i, j);
-                }
-            }
-            day++;
-        }
-//    }
-        for (int i = 0; i < box.length; i++) {
-            System.out.println();
-            for (int j = 0; j < box[i].length; j++) {
-                System.out.print(box[i][j] + " ");
-            }
-        }
 
+//        for (int i = 0; i < box.length; i++) {    // 과거 1
+//            for (int j = 0; j < box[0].length; j++) {
+//                if (box[i][j] == 1) {
+//                    q.offer(new Node(i, j));
+//                }
+//            }
+//        }
+
+        bfs(q);
+
+//        for (int i = 0; i < box.length; i++) {    // 출력테스트
+//            for (int j = 0; j < box[0].length; j++) {
+//                System.out.print(checked[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i < box.length; i++) {    // 과거 2
+//            for (int j = 0; j < box[0].length; j++) {
+//                if (day < box[i][j]) {
+//                    day = box[i][j];
+//                }
+//            }
+//        }
         checkFalse();
-        System.out.println(day);
+        System.out.println(day - 1);
     }
 
     private static void checkFalse() {
-        for (int i = 0; i < checked.length; i++) {
-            for (int j = 0; j < checked[i].length; j++) {
-                if (!checked[i][j]) {
-                    day = -1;
-                    break;
+        for (int i = 0; i < box.length; i++) {
+            for (int j = 0; j < box[0].length; j++) {
+                if (box[i][j] == 0) {
+                    day = 0;
+                    return;
                 }
             }
         }
     }
 
-    private static void bfs(int x, int y) {
-        Node start = new Node(x, y);
-        checked[x][y] = true;
+    private static void bfs(Queue<Node> q) {
 
-        for (int i = 0; i < 4; i++) {
-            int nx = start.x + dx[i];
-            int ny = start.y + dy[i];
-
-            if (nx < 0 || ny < 0 || nx >= box.length || ny >= box[x].length || checked[nx][ny]) {
-                continue;
+        while (!q.isEmpty()) {
+            Node start = q.poll();
+            int x = start.x;
+            int y = start.y;
+//            checked[x][y] = true; // 과거 3
+            if (box[x][y] > day) {  // 과거 2
+                day = box[x][y];
             }
 
-            if (box[nx][ny] == 0) {
-                box[nx][ny] = 1;
-                checked[nx][ny] = true;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= box.length || ny >= box[0].length) {
+//                        || checked[nx][ny]) { // 과거 3
+                    continue;
+                }
+
+//                if (box[nx][ny] == -1) {  // 과거 3
+//                    checked[nx][ny] = true;
+//                    continue;
+//                }
+
+                if (box[nx][ny] == 0) {
+                    box[nx][ny] = day + 1; // 과거 2
+//                    box[nx][ny] = box[x][y] + 1;  // 과거 2
+//                    checked[nx][ny] = true;   // 과거 3
+                    Node next = new Node(nx, ny);
+                    q.offer(next);
+                }
             }
         }
     }
@@ -83,12 +111,15 @@ public class 토마토 {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         box = new int[b][a];
-        checked = new boolean[b][a];
+//        checked = new boolean[b][a];  // 과거 3
 
         for (int i = 0; i < b; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < a; j++) {
                 box[i][j] = Integer.parseInt(st.nextToken());
+                if (box[i][j] == 1) {   // 과거 1
+                    q.offer(new Node(i, j));
+                }
             }
         }
     }

a42fea05ef0f56ea4b1172045b5036c1f303aada
BMDK9 <qnrakdlzm77@naver.com>
2024-04-23 18:25:02 +0900

    2024-04-23

--- src/baeckJun/array/최소최대.java
+++ src/baeckJun/array/최소최대.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class MinMax {
+public class 최소최대 {
 
     public static void main(String[] args) throws IOException {
         // 배열 안쓰고

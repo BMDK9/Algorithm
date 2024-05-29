569903907d25a9d781d5a3ce275afbfca7f255aa
BMDK9 <qnrakdlzm77@naver.com>
2024-05-29 20:13:11 +0900

    2024-05-29

---
+++ src/baeckJun/ㅍ정렬/좌표정렬하기2.java
@@ -0,0 +1,39 @@
+package baeckJun.ㅍ정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class 좌표정렬하기2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] coord = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            coord[i][0] = Integer.parseInt(st.nextToken());
+            coord[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(coord, (e1, e2) -> {
+            if (e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            }
+            return e1[1] - e2[1];
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(coord[i][0]).append(' ').append(coord[i][1]).append('\n');
+        }
+
+        String str = sb.toString();
+        System.out.println(str);
+    }
+}

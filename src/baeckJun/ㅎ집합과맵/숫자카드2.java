57be143f62c9d14271259a5d8f9e5dd500f92819
김성훈 <qnrakdlzm77@naver.com>
2026-03-27 23:49:33 +0900

    2026-03-27 숫자카드2 리팩터링

--- src/baeckJun/ㅎ집합과맵/숫자카드2.java
+++ src/baeckJun/ㅎ집합과맵/숫자카드2.java
@@ -1,39 +1,32 @@
 package baeckJun.ㅎ집합과맵;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 public class 숫자카드2 {
-  public static void main(String[] args) {
-    try {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      br.readLine();
-      String[] CardNumbers = br.readLine().split(" ");
-      br.readLine();
-      String[] reqNumbers = br.readLine().split(" ");
+    br.readLine();
+    String[] cardNumbers = br.readLine().split(" ");
+    br.readLine();
+    String[] reqNumbers = br.readLine().split(" ");
 
-      
-      LinkedHashMap<String, Integer> answer = new LinkedHashMap<>();
-      for (String reqNumber : reqNumbers) {
-        answer.put(reqNumber, 0);
-        for (String cardNumber : CardNumbers) {
-          if (reqNumber.equals(cardNumber)) {
-            answer.put(reqNumber, answer.get(reqNumber) + 1);
-          }
-        }
-      }
-
-      StringBuilder sb = new StringBuilder();
-      for (Integer value : answer.values()) {
-          sb.append(value).append(" ");
-      }
-
-      System.out.println(sb.toString().trim());
-    } catch (Exception e) {
-      e.printStackTrace();
+    Map<Integer, Integer> cardCnt = new HashMap<>();
+    for (String num : cardNumbers) {
+      int n = Integer.parseInt(num);
+      cardCnt.put(n, cardCnt.getOrDefault(n, 0) + 1);
     }
-    
+
+    StringBuilder sb = new StringBuilder();
+    for (String req : reqNumbers) {
+      int n = Integer.parseInt(req);
+      sb.append(cardCnt.getOrDefault(n, 0)).append(" ");
+    }
+
+    System.out.println(sb.toString().trim());
   }
 }
